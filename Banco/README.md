d7cecc889717a3bcefdde982bf77cb2ab0ccf5d1
beatriz_ambrosio <beamb0694@gmail.com>
2024-04-19 18:07:59 -0300

    funcoes de cc e add usuario e melhorias na ui

--- Banco/README.md
+++ Banco/README.md
@@ -5,11 +5,26 @@
     Primeiro desafio de projeto da trilha de Python AI Backend Developer.
     Utilizando Python 3.11.9
 
+    página da trilha: https://web.dio.me/track/coding-future-vivo-python-ai-backend-developer
+
 ### ❎ Objetivo
-    Criação de três funcionalidades
+    v1 - Criação de três funcionalidades
     Saque: Verificar se a quantidade do saque não é maior que a quantia em banco. O saque só pode ser de no máximo 500 reais, e no máximo 3 saques por dia.
     Depósito: Adicionar dinheiro ao banco
     Extrato: Mostrar todas as operações realizadas sequencialmente e com o saldo atual da conta no final do extrato
+
+    v2 - Criação de novas funcionalidades
+    Conta Corrente = possibilidade de criação de conta corrente apenas vinculando com o cpf do usuario previamente criado.  
+    Criação de Usuario = criação de usuário com nome, cpf, e endereço completo
+    Listar usuários e conta correntes
+    
+    Atualização:
+        - Função saque: Deve receber argumentos apenas por nome. Retorno: saldo e extrato
+        - Função deposito: Deve receber argumentos apenas por posição. Retorno: saldo e extrato
+        - Função extrato: Deve receber argumentos por posição e nome.
+        - Melhorias gerais na UI.
+
+    
 
 ### 🌄 Futuro
     Conforme o andamento do curso, esse programa terá novas funcionalidades no futuro.
@@ -17,5 +32,10 @@
 
 ### Versão
 
-V.1
+#### V.1
     Funcionalidades de saque, deposito e extrato.
+
+#### V.2
+    - Funções criar conta, criar conta corrente adicionadas
+    - Funções de listar todas as contas e todos os usuários cadastrados
+    - Atualizado código para funções saque, depósito e extrato e melhoria de desempenho
--- Banco/banco.py
+++ Banco/banco.py
@@ -1,17 +1,118 @@
-
-menu = """\n
- --- MENU --- 
-1 - Depositar
-2 - Sacar
-3 - Extrato
-4 - Sair\n
-
-Digite a opção escolhida: """
 saldo = 0
-limite = 500
+LIMITE = 500
 extrato = ""
 numero_saque = 0
-max_saque = 3
+MAX_SAQUE = 3
+list_user = []
+list_cc = []
+
+def func_saque(*,saldo, qtd_saque, extrato, LIMITE, numero_saque):
+    if numero_saque <= MAX_SAQUE:
+        if qtd_saque <= LIMITE:
+            if qtd_saque <= saldo:
+                saldo = (saldo - qtd_saque)
+                numero_saque +=1
+                extrato += (f"Saque: R${qtd_saque:.2f}\n")
+            else:
+                print("Saque maior que o Saldo")
+        else:
+            print("Max de R$500,00 por saque")
+    else:
+        print("Limite de Saque diário")
+    return saldo, extrato
+
+def func_deposito(saldo, ver_saldo, extrato, /):
+    if ver_saldo >= 0: 
+        saldo += ver_saldo
+        extrato += (f'Depósito: R$ {ver_saldo}\n')
+    else:
+        print(f"Valor Errado. Valor: R${saldo}")
+    return saldo, extrato
+
+def func_extrato(saldo,/,*, extrato):
+    return print(f"\nExtrato\n{extrato}\n Saldo atual: R${saldo:.2f}")
+
+def criar_cc(cpf):
+    AGENCIA = '0001'
+    inc_conta = (len(list_cc) + 1)
+    new_cc = dict(user_cpf = cpf, conta = inc_conta, agencia = AGENCIA)
+    user_exists = filtrar_cpf(cpf, list_user)
+    if user_exists:
+        new_cc.update(user_exists)
+        list_cc.append(new_cc)
+    else:
+        print(f'cpf nao esta cadastrado, criar o usuario')
+
+    print(f"""
+          ================ CONTA CORRENTE ==============  
+          \tNome: {new_cc['nome_user']},
+          \tAgencia: {new_cc['agencia']},
+          \tConta Corrente: {new_cc['conta']}\n
+        """)
+    
+    return list_cc
+
+def criar_client(nome, dt_nasc, cpf, endereco):
+    new_user = dict(nome_user=nome, dt_nasc_user=dt_nasc, cpf_user=cpf, endereco_user=endereco)
+    if len(list_user) == 0:
+        list_user.append(new_user)
+    else:
+        user_exists = filtrar_cpf(cpf, list_user)
+        if user_exists:
+            print(f"CPF {user_exists['cpf_user']} ja registrado")
+        else:
+            list_user.append(new_user)
+
+        print(f"""
+            ========= USUARIO CRIADO COM SUCESSO =========
+            \tNome: {new_user['nome_user']},
+            \tData de Nascimento: {new_user['dt_nasc_user']},
+            \tCPF: {new_user['cpf_user']},
+            \tEndereço: {new_user['endereco_user']}
+            """)  
+        
+    return list_user
+
+def listar_cc(list_cc):
+    for each in list_cc:
+        print(f"""
+            \tNome: {each['nome_user']},
+            \tAgencia: {each['agencia']},
+            \tConta Corrente: {each['conta']}
+            \t====================================
+        """)
+
+def listar_client(list_user):
+    for each in list_user:
+        print(f"""
+            \tNome: {each['nome_user']},
+            \tData de Nascimento: {each['dt_nasc_user']},
+            \tCPF: {each['cpf_user']},
+            \tEndereço: {each['endereco_user']}
+            \t====================================
+        """)
+
+def filtrar_cpf(cpf, list_user):
+    for each in list_user:
+        if each["cpf_user"] == cpf:
+            found_user = each
+        else:
+            found_user = []
+    return found_user
+
+menu = """
+ --- MENU --- 
+
+1 - Sacar
+2 - Deposito
+3 - Extrato
+4 - Criar Conta Corrente 
+5 - Criar Cliente
+6 - Todas CC
+7 - Todos Usuarios
+8 - Sair
+
+Digite a opção escolhida: """
 
 
 print(
@@ -23,34 +124,51 @@ while True:
     opcao = input(menu)
 
     if opcao == "1":
+        print("\n--- Sacar ---")
+        qtd_saque = float(input("Valor a sacar: R$\n"))
+        saldo, extrato = func_saque(saldo, qtd_saque, extrato, LIMITE, numero_saque)
+        print(f"Saldo: {saldo}")
+        
+    elif opcao == "2":
         print("\n--- Depósito ---")
         ver_saldo = float(input("Valor a depositar: R$"))
-        if ver_saldo >= 0: 
-            saldo = ver_saldo + saldo
-            extrato += (f'Depósito: R$ {ver_saldo}\n')
-            print(f"Saldo atualizado: R${saldo:.2f}\n")
-        else:
-            print(f"Valor Errado. Valor: R${saldo}")
-    elif opcao == "2":
-        print("\n--- Sacar ---")
-        if numero_saque <= max_saque:
-            qtd_saque = float(input("Valor a sacar: R$\n"))
-            if qtd_saque <= limite:
-                if qtd_saque <= saldo:
-                    saldo = (saldo - qtd_saque)
-                    numero_saque +=1
-                    extrato += (f"Saque: R${qtd_saque:.2f} | Qtd de Saques(max 3 diário): {numero_saque}\n")
-                else:
-                    print("Saque maior que o Saldo")
-            else:
-                print("Max de R$500,00 por saque")
-        else:
-            print("Limite de Saque diário")
-        print(f"Voce fez {numero_saque} saque. Saldo Atualizado: R${saldo:.2f}\n")
+        saldo, extrato = func_deposito(saldo, ver_saldo, extrato)
+        print(f"Saldo atualizado: R${saldo:.2f}\n")
 
     elif opcao == "3":
-        print(f"\nExtrato\n{extrato}\n Saldo atual: R${saldo:.2f}")
-           
+        print("\n--- Extrato ---")
+        func_extrato(saldo, extrato)
+
     elif opcao == "4":
+        print("\n--- Criar Conta Corrente ---")   
+        user_cc = input("Digite o CPF do usuário: ") 
+        cc = criar_cc(user_cc)
+                
+    elif opcao == "5":
+        print("\n--- Criar Cliente ---") 
+        nome = str(input("Nome: "))
+        dt_nasc = input("Data de Nascimento: ")
+        cpf = input("CPF: ")
+        
+        logradouro = str(input("Logradouro: "))
+        nro = input("Número: ")
+        bairro = str(input("Bairro: "))
+        cidade = str(input("Cidade: "))
+        estado =  str(input("Estado: "))
+        
+        cidade_estado = cidade, estado
+        cidade_estado = "/".join(cidade_estado)
+        endereco = logradouro, nro, bairro, cidade_estado
+        endereco = " - ".join(endereco)
+
+        cliente = criar_client(nome, dt_nasc, cpf, endereco)
+
+    elif opcao == "6":
+        print("Listar Todas as Contas Correntes")
+        listar_cc(cc)
+    elif opcao == "7":    
+        print("Listar todos os Usuários")
+        listar_client(cliente)
+    elif opcao == "8":
         print("Sair do Sistema")
         break;
